f2245d6cc370a06a58282235b82a440252438468
Andrea <andreamayhirji@gmail.com>
2018-10-23 23:42:36 +0000

    add a class called Pizza, added some functions inside to constructo a pizza and add toppings.

--- wtf-class_instances.js
+++ wtf-class_instances.js
@@ -4,10 +4,16 @@
 //We can use any name for the class but a class name should always be a noun, and the first letter should always be capitalized.
 
 class Pizza {
-
+    constructor() {
+        this.topping = ['cheese'];
+    }
+    addTopping(topping) {
+        this.toppings.push(topping);
+    }
 }
 
+//To create a new object from a class, we use the new keyowrd. 
+// pizza1 and pizza2 are pizza objects.
+//when you create a new object from a class it is an instance.
 let pizza1 = new Pizza();
 let pizza2 = new Pizza();
-
-
